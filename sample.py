03608785dcbf9f2a3f836ba082bdbb383ddec402
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-11-20 02:56:51 +0900

    add sample

--- sample.py
+++ sample.py
@@ -4,4 +4,15 @@ from getCryptoCurrencyData import CryptoCurrencyData
 
 
 print(CryptoCurrencyData("ethereum").getRate("1D"))
+
+
+CryptoCurrencyData("bitcoin", proxies = {}, headers = {}).data
+CryptoCurrencyData("ethereum", proxies = {
+     "http":"http://example.com/proxy.pac",
+     "https":"http://example.com/proxy.pac"
+
+}).getRate("1D")
+next(reversed(CryptoCurrencyData("ethereum").getRate("1D")["data"]["points"].values()), None)["c"][0]
+
+
 # ["1D","7D","1M","3M","1Y","YTD","ALL"]
